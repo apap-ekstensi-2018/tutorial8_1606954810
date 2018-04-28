df105ec0a0076e41a5bda6ab3785bfffd5e499db
ganigemilar <ganigemilar10@gmail.com>
2018-04-28 11:22:53 +0700

    tutorial 8

---
+++ src/main/java/com/example/tutorial8.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class tutorial8
+{
+
+    public static void main (String[] args)
+    {
+        SpringApplication.run (tutorial8.class, args);
+    }
+}
